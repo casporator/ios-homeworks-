6b9865252295133171a8434ee35720b19aace7e6
Олег Попов <casporator123@gmail.com>
2022-08-26 23:41:33 +0300

    final version commit

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,10 +9,7 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
-    
-    
     private let image: UIImageView = {
-        // устанавливаю согласно макету (но размер на макете не указан!)
         let photo = UIImageView(frame: CGRect(x: 16, y: 16, width: 120, height: 120))
         photo.image = UIImage(named: "IMG_0037")
         photo.layer.cornerRadius = 60
@@ -43,12 +40,10 @@ class ProfileHeaderView: UIView {
         button.layer.cornerRadius = 4
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        //настраиваем тени
         button.layer.shadowOffset = CGSize(width: 4, height: 4)
         button.layer.shadowRadius = 4
         button.layer.shadowColor = UIColor.black.cgColor
         button.layer.shadowOpacity = 0.7
-        //почему он ругается на self? в InfoViewController я использовал точно такой же метод и там не было желтого знака...
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         
         return button
@@ -64,18 +59,15 @@ class ProfileHeaderView: UIView {
         return statusLbl
     }()
     
-
     private let textField : UITextField = {
         let txtField = UITextField()
         txtField.font = UIFont(name: "regular", size: 15.0)
         txtField.placeholder = "Enter you status"
         txtField.backgroundColor = .white
         txtField.translatesAutoresizingMaskIntoConstraints = false
-        
         txtField.layer.borderWidth = 1
         txtField.layer.cornerRadius = 12
         txtField.layer.borderColor = UIColor.black.cgColor
-        
         txtField.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
         return txtField
     }()
@@ -83,22 +75,9 @@ class ProfileHeaderView: UIView {
     
     var statusText: String = ""
     
-    
-    /*тут у меня была засада полная! ничего не работало пока мне ребята из группы не подсказали
-     написать:
-    
-     override init(frame: CGRect) {
-     super.init(frame: frame)
-     }
-     required init?(coder: NSCoder) {
-         super.init(coder: coder)
-     }
-     я в замешательстве...очевидно что это инициализатор, но почему и зачем?
-     */
-   
     override init(frame: CGRect) {
         super.init(frame: frame)
-
+        
         addViews()
         addConstraints()
     }
@@ -106,14 +85,12 @@ class ProfileHeaderView: UIView {
         super.init(coder: coder)
     }
     
-    
-    
     @objc func buttonPressed() {
         
         if statusText != "" {
             statusLabel.text = statusText
             print (statusText)
-     }
+        }
     }
     
     @objc func statusTextChanged(_ textField: UITextField){
@@ -134,21 +111,13 @@ func addConstraints(){
         button.topAnchor.constraint(equalTo: image.bottomAnchor, constant: 16),
         
         statusLabel.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 16),
-        
-        /* тут по условию макета надо сделать отступ от кнопки -34, НО!! при выполнении задания
-        со звездой я должен вставить между ними тестовую панель, а она по условию макета имеет
-        ширину 40! так что я этот момент не понял и отступ увеличил до -54
-        */
         statusLabel.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -54),
         
         textField.heightAnchor.constraint(equalToConstant: 40),
         textField.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -10),
         textField.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
         textField.leftAnchor.constraint(equalTo: image.rightAnchor, constant: 16)
-        
-        
-       ])
-    
+        ])
     }
    
     func addViews(){
@@ -158,7 +127,4 @@ func addConstraints(){
         addSubview(statusLabel)
         addSubview(textField)
     }
-    
-    
-    
 }
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -9,58 +9,49 @@ import UIKit
 
 
 class ProfileViewController: UIViewController {
-    
-    
-    
-    
-    
     let profileView: UIView = {
         let view = ProfileHeaderView()
-
+        
         return view
     }()
     
-   override func viewDidLoad() {
-           super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-
-       view.backgroundColor = .lightGray
-       
-       // создаем тайтл заголовка;
-       self.title = "Профиль"
-       
-       
-       view.addSubview(profileView)
-       viewWillLayoutSubviews()
-       navBarCustomization()
-     
-   }
+        view.backgroundColor = .lightGray
+        
+        // создаем тайтл заголовка;
+        self.title = "Профиль"
+        
+        view.addSubview(profileView)
+        viewWillLayoutSubviews()
+        navBarCustomization()
+    }
     
     // создаю "чёлку" навигационного бара
     //правда я надеялся что координаты X и Y будут идти от бара, НО нет ((( все-равно от верхнего правого угла
     func navBarCustomization () {
-            let appearance = UINavigationBarAppearance()
-            appearance.backgroundColor = .systemBackground
-            appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
-            appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
-            navigationController?.navigationBar.tintColor = .brown
-            navigationController?.navigationBar.standardAppearance = appearance
-            navigationController?.navigationBar.compactAppearance = appearance
-            navigationController?.navigationBar.scrollEdgeAppearance = appearance
-           
-
-            self.navigationItem.title = "Профиль"
-        }
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .systemBackground
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
+        navigationController?.navigationBar.tintColor = .brown
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        
+        self.navigationItem.title = "Профиль"
+        
+        // добавляю кнопку справа нав бара (это так, для саморазвития)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .edit)
+        self.navigationItem.rightBarButtonItem?.tintColor = .black
+    }
     
     //в методе viewWillLayoutSubviews() задайте ему frame, равный frame корневого view
 
  override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-     
-    
-     /* тут так же проблема возникла. Я растянул на весь экран, но с отступом 90 для чёлки навбара
-     - всю голову сломал как сделать (я очень расчитаывал, что навбар является как бы системным элементом и координаты будут автоматически идти от него )
-     */
      
      profileView.frame = CGRect(x:0, y: 90, width: view.frame.width, height: view.frame.height)
      
